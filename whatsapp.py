0bd4d9e51fe3b897330b1fb7c599a4306d6d5f3f
Rafael Miranda <102253663+rafarioRafael@users.noreply.github.com>
2025-06-03 23:20:50 -0300

    adding csv contacts

--- whatsapp.py
+++ whatsapp.py
@@ -1,3 +1,6 @@
+import csv
+import json
+import random
 import sys
 import time
 from selenium import webdriver
@@ -9,8 +12,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 
-user_data_dir = "C:/Users/ENV_VARIABLE/AppData/Local/Google/Chrome/User Data"
-profile_dir = "Default"
+#user_data_dir = "C:/Users/rafael.miranda/AppData/Local/Google/Chrome/User Data"
+#profile_dir = "Default"
 
 #keeping the data (cookies, cache, etc.) from the browser
 
@@ -21,33 +24,84 @@ options.add_argument("--profile-directory=Default")
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
+def choose_message(path_json="frases/goodnight.json"):
+    with open(path_json, encoding='utf-8') as file:
+        dados = json.load(file)
+        return random.choice(dados["frases"])
+
+def load_contacts(path_csv="contatos.csv"):
+    contatos = []
+    with open(path_csv, encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for linha in reader:
+            contatos.append(linha['nome'])
+    return contatos
+
+def buscar_contato(contato):
+    print("Buscando contato...")
+
+    try:
+        # Localiza e clica na caixa de busca
+        search_box_xpath = '//div[@contenteditable="true"][@data-tab="3"]'
+        search_box = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, search_box_xpath))
+        )
+        search_box.clear()
+        search_box.click()
+        time.sleep(1)
+        search_box.send_keys(contato)
+        time.sleep(2)
+
+        # Clica no contato encontrado
+        contato_xpath = f'//span[@title="{contato}"]'
+        contato_selecionado = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, contato_xpath))
+        )
+        contato_selecionado.click()
+        print("Contato selecionado!")
+
+        return True
+    except Exception as e:
+        print(f"Contato '{contato}' não encontrado ou não foi possível clicar.")
+        return False
+
 try:
     print('Abrindo o zap')
     driver.get('https://web.whatsapp.com/')
     time.sleep(15)
     
-    contato = '' #Nome do contato
-    palavras = '' #Mensagem para enviar
-    #da para fazer uma lista com as palavras tbm, por isso foi usafo o for na linha 39
-  
-    print("Selecionando o  contato...")
-    contato_xpath = f'//*[@id="side"]//span[contains(text(), "{contato}")]'
-    contato_selecionado = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, contato_xpath))
-    )
-    contato_selecionado.click()
-    print("Contato selecionado!")
+    contatos = load_contacts() #Nome do contato
+    for contato in contatos:
+        mensagem = choose_message() 
+        print(f"Enviando para {contato}: {mensagem}")
 
-    print('Enviando mensagem...')
-    message_box_xpath = '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div[2]/div[1]/p'
-    message_box = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, message_box_xpath))
-    )
-    message_box.click()
-    message_box.send_keys(palavras)
-    message_box.send_keys(Keys.RETURN)
+        if buscar_contato(contato):
+            try:
+                #Mensagem para enviar
+                print("Selecionando o  contato...")
+                contato_xpath = f'//*[@id="side"]//span[contains(text(), "{contato}")]'
+                contato_selecionado = WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.XPATH, contato_xpath))
+                )
+                contato_selecionado.click()
+                time.sleep(3)
+                #Enviando mensagem
+                message_box_xpath = '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div[2]/div[1]/p'
+                message_box = WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.XPATH, message_box_xpath))
+                )
+                message_box.click()
+                message_box.send_keys(mensagem)
+                message_box.send_keys(Keys.RETURN)
+                time.sleep(3)
+            except Exception as e:
+                print(f"Erro ao enviar mensagem para {contato}: {e}")
+    else:
+        print(f"Pulo o contato {contato} pois não foi possível localizá-lo.")
+            
 
     print("Mensagem enviada com sucesso!")
     time.sleep(5)
 except Exception as e:
     print(f"Ocorreu um erro: {e.with_traceback}")
+
